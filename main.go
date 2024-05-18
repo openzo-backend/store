4c28111d7ddea7446adbc690ac5bb8d894c8ca82
tanush <tanush@myyahoo.com>
2024-05-18 17:26:43 +0530

    kafka added

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+
+	"bufio"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tanush-128/openzo_backend/store/config"
@@ -12,11 +15,47 @@ import (
 	"github.com/tanush-128/openzo_backend/store/internal/repository"
 	"github.com/tanush-128/openzo_backend/store/internal/service"
 	"google.golang.org/grpc"
+
+	"os"
+	"strings"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 var UserClient pb.UserServiceClient
 
 type User2 struct {
+}
+
+func ReadConfig() kafka.ConfigMap {
+	// reads the client configuration from client.properties
+	// and returns it as a key-value map
+	m := make(map[string]kafka.ConfigValue)
+
+	file, err := os.Open("client.properties")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to open file: %s", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if !strings.HasPrefix(line, "#") && len(line) != 0 {
+			kv := strings.Split(line, "=")
+			parameter := strings.TrimSpace(kv[0])
+			value := strings.TrimSpace(kv[1])
+			m[parameter] = value
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Failed to read file: %s", err)
+		os.Exit(1)
+	}
+
+	return m
 }
 
 func main() {
@@ -31,12 +70,26 @@ func main() {
 		log.Fatal(fmt.Errorf("failed to connect to database: %w", err))
 	}
 
-	// // Initialize gRPC server
-	// grpcServer := grpc.NewServer()
-	// Storepb.RegisterStoreServiceServer(grpcServer, service.NewGrpcStoreService(StoreRepository, StoreService))
-	// reflection.Register(grpcServer) // Optional for server reflection
+	conf := ReadConfig()
+	p, _ := kafka.NewProducer(&conf)
+	// topic := "notification"
 
-	//Initialize gRPC client
+	// go-routine to handle message delivery reports and
+	// possibly other event types (errors, stats, etc)
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Failed to deliver message: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Produced event to topic %s: key = %-10s value = %s\n",
+						*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
+				}
+			}
+		}
+	}()
+
 	conn, err := grpc.Dial(cfg.UserGrpc, grpc.WithInsecure())
 
 	if err != nil {
@@ -57,9 +110,7 @@ func main() {
 	storeRepository2 := repository.NewStoreRepository(db)
 	StoreService := service.NewStoreService(storeRepository, imageClient)
 
-	go service.GrpcServer(cfg, &service.Server{
-		StoreRepository: storeRepository2,
-	})
+	go consumeKafka(storeRepository2, p)
 
 	// Initialize HTTP server with Gin
 	router := gin.Default()
@@ -73,17 +124,80 @@ func main() {
 
 	// router.Use(middlewares.JwtMiddleware(c))
 	router.POST("/", handler.CreateStore)
-	router.GET("/:id", handler.GetStoreByID)
-
 	router.GET("/pincode/:pincode", handler.GetStoresByPincode)
 	router.GET("/pincode/:pincode/category/:category", handler.GetStoresByPincodeAndCategory)
 	router.GET("/phone/:phone_no", handler.GetStoreByPhoneNo)
 	router.GET("/getCategories", handler.GetCategories)
 	router.Use(middlewares.NewMiddleware(c).JwtMiddleware)
+	router.GET("/:id", handler.GetStoreByID)
 	router.PUT("/:id", handler.UpdateStore)
 
 	// router.Use(middlewares.JwtMiddleware)
 
 	router.Run(fmt.Sprintf(":%s", cfg.HTTPPort))
+
+}
+
+type Notification struct {
+	Message  string `json:"message"`
+	FCMToken string `json:"fcm_token"`
+}
+
+func consumeKafka(storeRepo repository.StoreRepository, notificationProducer *kafka.Producer) {
+	conf := ReadConfig()
+
+	topic := "onlineorder"
+
+	// sets the consumer group ID and offset
+	conf["group.id"] = "go-group-1"
+	conf["auto.offset.reset"] = "earliest"
+
+	// creates a new consumer and subscribes to your topic
+	consumer, _ := kafka.NewConsumer(&conf)
+	consumer.SubscribeTopics([]string{topic}, nil)
+	var order struct {
+		StoreId string `json:"store_id"`
+	}
+	run := true
+	for run {
+		// consumes messages from the subscribed topic and prints them to the console
+		e := consumer.Poll(1000)
+		switch ev := e.(type) {
+		case *kafka.Message:
+			// application-specific processing
+			err := json.Unmarshal(ev.Value, &order)
+			if err != nil {
+				fmt.Println("Error unmarshalling JSON: ", err)
+			}
+			fmt.Println("Order received: ", order)
+
+			fcm, err := storeRepo.GetFCMTokenByStoreID(order.StoreId)
+			if err != nil {
+				fmt.Println("Error getting FCM token: ", err)
+			}
+			fmt.Println("FCM token: ", fcm)
+
+			notificationMsg, _ := json.Marshal(Notification{
+				Message:  "You have a new order",
+				FCMToken: fcm,
+			})
+			notificationTopic := "notification"
+
+			// send a notification to the store
+			notificationProducer.Produce(&kafka.Message{
+				TopicPartition: kafka.TopicPartition{Topic: &notificationTopic, Partition: kafka.PartitionAny},
+				Value:          notificationMsg,
+			}, nil)
+
+			notificationProducer.Flush(15 * 1000)
+
+		case kafka.Error:
+			fmt.Fprintf(os.Stderr, "%% Error: %v\n", ev)
+			run = false
+		}
+	}
+
+	// closes the consumer connection
+	consumer.Close()
 
 }
