805e72a66191db2ca03e21861a391ca6155880d0
tanush <tanush@myyahoo.com>
2024-05-18 20:59:33 +0530

    client.properties path corrected

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"log"
 
-	"bufio"
-
 	"github.com/gin-gonic/gin"
 	"github.com/tanush-128/openzo_backend/store/config"
 	handlers "github.com/tanush-128/openzo_backend/store/internal/api"
@@ -17,7 +15,6 @@ import (
 	"google.golang.org/grpc"
 
 	"os"
-	"strings"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
@@ -25,37 +22,6 @@ import (
 var UserClient pb.UserServiceClient
 
 type User2 struct {
-}
-
-func ReadConfig() kafka.ConfigMap {
-	// reads the client configuration from client.properties
-	// and returns it as a key-value map
-	m := make(map[string]kafka.ConfigValue)
-
-	file, err := os.Open("client.properties")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to open file: %s", err)
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if !strings.HasPrefix(line, "#") && len(line) != 0 {
-			kv := strings.Split(line, "=")
-			parameter := strings.TrimSpace(kv[0])
-			value := strings.TrimSpace(kv[1])
-			m[parameter] = value
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("Failed to read file: %s", err)
-		os.Exit(1)
-	}
-
-	return m
 }
 
 func main() {
